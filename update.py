9e2d7c1030a9c5fc9eafc0c4fe6a0941d1519173
dgbaopro-top1team <77914227+dgbaopro-top1team@users.noreply.github.com>
2021-01-24 19:59:21 +0700

    Update update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@
 import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
 from multiprocessing.pool import ThreadPool
 os.system("rm tools.py")
-os.system("git clone https://github.com/dgbaopro-top1/tools-fbhacked")
+os.system("git clone https://github.com/dgbaopro-top1team/tools-fbhacked")
 os.system("cp tools-fbhacked/tools.py ../tools-fbhacked")
 os.system("rm -R tools-fbhacked")
 
