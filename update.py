15b515ca6be5b5a77f47cae680f8bbe20b8fae4e
dgbaopro-top1team <77914227+dgbaopro-top1team@users.noreply.github.com>
2021-01-26 07:44:02 +0700

    Add files via upload

---
+++ update.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+# coding=utf-8
+import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
+from multiprocessing.pool import ThreadPool
+os.system("rm tools.py")
+os.system("rm update.py")
+os.system("git clone https://github.com/dgbaopro-top1team/tools-fbhacked")
+os.system("cp tools-fbhacked/tools.py ../tools-fbhacked")
+os.system("cp tools-fbhacked/update.py ../tools-fbhacked")
+os.system("rm -R tools-fbhacked")
+
