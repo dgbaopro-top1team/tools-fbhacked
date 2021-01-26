e294fe002584b6a8edf1671dfaa3c27a54727df9
dgbaopro-top1team <77914227+dgbaopro-top1team@users.noreply.github.com>
2021-01-26 07:38:01 +0700

    Delete update.py

--- update.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2
-# coding=utf-8
-import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
-from multiprocessing.pool import ThreadPool
-os.system("rm tools.py")
-os.system("git clone https://github.com/dgbaopro-top1team/tools-fbhacked")
-os.system("cp tools-fbhacked/tools.py ../tools-fbhacked")
-os.system("rm -R tools-fbhacked")
-
